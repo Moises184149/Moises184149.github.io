3a5a81b9e33f7def01bf6ac31acd38e99f5c7d3e
Moises184149 <159570801+Moises184149@users.noreply.github.com>
2024-05-14 16:27:16 -0600

    Add files via upload

---
+++ script/funciones.js
@@ -0,0 +1,256 @@
+function cargaPractica(htmlDocument) {
+	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
+}
+function mostrarTextoLAG() { //Texto de Lag
+	document.getElementById('content').innerHTML = 
+	`<h2>Licenciatura en Administración y Gestión (LAG)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> La Licenciatura en Administración y Gestión tiene por objetivo formar profesionistas con capacidades gerenciales altamente competitivos que respondan a los desafíos a los que se enfrentan las organizaciones en ambientes de incertidumbre, dirigiendo eficazmente sus recursos y funciones, a través de una visión vanguardista para diseñar, evaluar y aplicar estrategias que permitan innovar o mejorar procesos en las organizaciones en un marco de sustentabilidad.</p>
+	<br>
+	<img src="img/Plan-Academico-LAG.jpg" alt="Imagen de LAG" width="600px">
+	
+	
+	`;
+	
+	
+}
+function mostrarTextoLMI() { // texto de LMI
+	document.getElementById('content').innerHTML = 
+	`<h2>Licenciatura en Mercadotecnia Internacional (LMI)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> Habilidad para la planeación, diseño e implementación de estrategias de comunicación de la empresa como su mercado: campañas de publicidad e imagen, promoción de ventas, ventas personales, relaciones públicas, entre otros.</p>
+	<br>
+	<img src="img/Plan-Academico-LMI.jpg" alt="Imagen de LMI" width="600px">
+	`;
+}
+function mostrarTextoITMA(){ //Texto de ITMA
+	document.getElementById('content').innerHTML = 
+	`<h2>Ingeniería en Tecnologías de Manufactura (ITMA)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> La carrera de Ingeniería en Tecnologías de Manufactura (ITMA), ha sido diseñada para cubrir diferentes funciones dentro del sector industrial, teniendo como sus tres pilares modulares, la automatización y control de procesos, el diseño avanzado y la administración de procesos. El conocimiento es transmitido a los estudiantes mediante el enfoque de competencias, permitiendo así, mejorar sus habilidades y destrezas tanto sociales como cognitivas para enfrentar los desafíos en diferentes ámbitos laborales.  Al inicio de su carrera, se tienen las asignaturas básicas que sustentan estos tres pilares mencionados, donde se establecen las leyes físicas, el razonamiento matemático y lógico, así como, la socialización del estudiante con su entorno.</p>
+	<br>
+	<img src="img/Plan-Academico-ITMA.jpg" alt="Imagen de ITMA" width="600px">
+	`;
+}
+function mostrarTextoITI(){ //Texto de ITTI
+	document.getElementById('content').innerHTML = 
+	`<h2>Ingeniería en Tecnologías de la Información (ITI)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> El Ingeniero en Tecnologías de la Información aplica sus sólidos conocimientos al diseño, desarrollo e instrumentación de soluciones informáticas que requieren las organizaciones, atendiendo las necesidades humanas derivadas de la interacción con la computadora. Es competente para mantener equipos de cómputo operando eficientemente y ofrece al usuario soluciones integrales a los problemas asociados con el área computacional.</p>
+	<br>
+	<img src="img/Plan-Academico-ITI.jpg" alt="Imagen de ITI" width="600px">
+	`;
+}
+function mostrarTextoISTI(){ //Texto de ISTI
+	document.getElementById('content').innerHTML = 
+	`<h2>Ingeniería en Sistemas y Tecnologías Industriales (ISTI)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> En Ingeniería en Sistemas y Tecnologías Industriales se busca preparar a los alumnos en las 5 grandes áreas que comprende la Ingeniería Industrial como lo son: Producción, Calidad, Suministros de Materiales, Productividad y Seguridad, todo bajo un enfoque sistémico y pertinente, con la transmisión del conocimiento más actual en razón de metodologías, herramientas y software aplicable a los procesos de producción y de servicios. Además de la tecnología, busca que los alumnos se preparen para administrar y dirigir al personal.</p>
+	<br>
+	<img src="img/Plan-Academico-ISTI.jpg" alt="Imagen de ISTI" width="600px">
+	`;
+}
+function mostrarTextoITEM(){ //Texto de ITEM
+	document.getElementById('content').innerHTML = 
+	`<h2>Ingeniería en Telematica (ITEM)</h2>
+	<p><strong>Título otorgado:</strong> Licenciado en Administración y Gestión.</p>
+	<p><strong>Duración:</strong> 9 semestres.</p>
+	<p><strong>Descripción carrera:</strong> En la carrera de Ingeniería en Telemática, se prepara a los estudiantes en tres grandes áreas del conocimiento que conforman la carrera y las cuales son:</p>
+
+	<p>a) Las redes de telecomunicaciones, en dicha área del conocimiento se incluyen los temas que tienen que ver con los sistemas de telecomunicaciones de la actualidad, como lo son las redes de área local, las redes de cobertura amplía, las redes convergentes de voz, datos y video, las redes de telefonía celular, los sistemas de microondas terrestres y vía satélite, así como los estándares para instalar la infraestructura de cableado estructurado.</p>
+
+	<p>b) Las tecnologías de la información, que es el conjunto de aplicaciones basadas en software y equipos informáticos que hacen uso de las redes de telecomunicaciones para la transmisión y ejecución de las mismas; entre un sinnúmero de aplicaciones  encontramos: la programación web, el diseño de bases de datos y de conocimientos, aplicaciones de voz y video, programación para dispositivos móviles y para el posicionamiento de vehículos.</p>
+
+	<p>c) Los sistemas embebidos, es un área del conocimiento en constante desarrollo industrial y se refiere a sistemas electrónicos que incluyen una micro-computadora, entradas y salidas estándares, conectividad y software. Esta micro-computadora puede adaptarse a casi cualquier tipo de aplicación, siendo su principal ventaja la movilidad y la conectividad; haciendo realidad el concepto de internet de las cosas. Los sistemas embebidos pueden obtener, procesar, transmitir y recibir información de diferentes fuentes incluyendo sensores y dispositivos típicos de entrada/salida. Las aplicaciones en este sector son ilimitadas, por ejemplo, el sector automotriz, los servicios de salud, la automatización de líneas de producción, diversión, comercio, ciudades inteligentes y educación</p>
+	<br>
+	<img src="img/Plan-Academico-ITEM.jpg" alt="Imagen de ITEM" width="600px">
+	`;
+}
+function mostrarTextoMision() { // texto de la mision y la vision
+	document.getElementById('content').innerHTML = 
+	`<h3>Misión</h3>
+	<p>La Universidad Politécnica de San Luis Potosí, está comprometida con la excelencia en la formación integral y humana, y con el aprendizaje, el desarrollo y la aplicación del nuevo conocimiento.</p>
+
+	<p>La Universidad Politécnica une a estudiantes, profesores y personal administrativo para crear una comunidad de la más alta calidad académica basada en un modelo educativo abierto y flexible, y comprometida con el progreso social y económico del estado y del país.</p>
+
+	<p>La Misión se promueve mediante la enseñanza y la guía tutorial de los estudiantes, así como a través de la investigación aplicada en áreas estratégicas para la economía moderna, siempre vinculada con las necesidades de la sociedad y sus sectores productivos.</p>
+
+	<h3>Vision</h3>
+	<p>Aspiramos a ser una Universidad consolidada en su tarea de formar profesionales competentes, que se distinguen por su aportación al desarrollo de México y de San Luis Potosí y cuentan con reconocimiento en sus campos de actividad profesional y en su vida comunitaria.</p>
+
+	<p>La Universidad Politécnica de San Luis Potosí representa un espacio de desarrollo intelectual que atrae el mejor talento, tanto de estudiantes como profesores, que se sienten orgullosos de pertenecer a esta comunidad académica.</p>
+	
+	<p>La Universidad aprovecha eficazmente la tecnología para el trabajo académico, manteniéndose a la vanguardia en la incorporación de nuevas tecnologías y utilizándolas en forma intensiva en sus procesos de formación, investigación y gestión.</p>
+	
+	<p>La Universidad desarrolla actividades estratégicas en el campo de la innovación tecnológica en las áreas de conocimiento que cultiva, siendo un participante activo del sistema de innovación de la región, realizando proyectos conjuntos con empresas y gobiernos y ofreciendo servicios de capacitación, consultoría y desarrollo tecnológico de alto nivel al sector empresarial. Forma parte de redes de colaboración académica, nacionales e internacionales, en las cuales contribuye en temas de vanguardia tecnológica.</p>
+	
+	<p>La Universidad se caracteriza por una intensa vida académica y cultural, que abre sus puertas cotidianamente a la sociedad para compartir y difundir con ella sus trabajos científicos, tecnológicos y culturales. </p>`
+	
+}
+function mostrarTextoCultura(){
+	document.getElementById('content').innerHTML =
+	`<h2>Cultura y Deportes</h2>
+	<p>Ser parte activa en el cumplimiento del compromiso de nuestra universidad con excelencia en la formación integral y humana, desarrollando y aplicando  nuevos conocimientos; mediante un programa de actividades deportivas incluyente y diversificado que permita a los estudiantes equilibrar su desarrollo académico, que provea  de experiencias y valores que coadyuven a su madurez y calidad profesional, mismas que consistirán en un sello distintivo de su paso por la universidad politécnica.</p>
+	<p>Las disciplinas que actualmente se ofrecen son:</p>
+	<ul>
+		<li>Futbol soccer</li>
+		<li>Futbol rapido</li>
+		<li>Basquetbol</li>
+		<li>Volibol</li>
+		<li>Tae kwon Do<br>
+			<img src="img/Tae-kwon-do.jpg" alt="Imagen de ITEM" width="100px">
+		</li>
+		<li>Karate</li>
+		<li>Ajedrez</li>
+		<li>Atletismo</li>
+		<li>DibujoDibujo</li>
+		<li>Volibol</li>
+		<li>Guitarra</li>
+		<li>Karate</li>
+		<li>Baile-fitnnes</li>
+		<li>Coro</li>
+		
+	</ul>`
+}
+function mostrarTextoContacto() {
+	document.getElementById('content').innerHTML =
+	`<h2>La Universidad brindará atención principalmente vía correo electrónico, ponemos a tu servicio las siguientes direcciones electrónicas:</h2>
+	<h3>Teléfono: (444) 870-21-00</h3>
+	<p>Rectoría: Dr. Mario Alberto Martínez Rojas
+	<a href=mailto:rectoria@upslp.edu.mx>rectoria@upslp.edu.mx</a></p>
+
+	<p>Vicerrectoría: Dra. Martha Angélica de la Rosa Hernández
+	<a href=mailto:vicerrectoria@upslp.edu.mx>vicerrectoria@upslp.edu.mx</a></p>
+
+	<p>Dirección de Administración y Finanzas: Dr. Fernando Rodrigo Palavicini Piña
+	<a href=mailto:daf@upslp.edu.mx>daf@upslp.edu.mx</a></p>
+	
+
+	<p>Servicios Escolares: Dra. Laura Medina Barboza
+	<a href=mailto:serviciosescolares@upslp.edu.mx>serviciosescolares@upslp.edu.mx</a><p>
+
+	<p>Comunicación y Promoción – Oferta Educativa: Dra. Yolanda Jara Centeno
+	<a href=mailto:comunicacionypromocion@upslp.edu.mx>comunicacionypromocion@upslp.edu.mx</a></p>
+
+	<p>Vinculación: Dra. Laura Medina Barboza
+	<a href=mailto:vinculacion@upslp.edu.mx>vinculacion@upslp.edu.mx</a></p>
+	
+
+	<p>Extensión Universitaria: Lic. Johana Saydee Ramírez Rojas: 
+	<a href=mailto:extension.universitaria@upslp.edu.mx>extension.universitaria@upslp.edu.mx</a></p>
+
+	<p>Coordinación de Ingeniería en Telemática -Redes y Telecomunicaciones-
+	(ITEM):<br> Dr. Juan Antonio Cabrera Ricos     
+	<a href=mailto:item-irtel@upslp.edu.mx>item-irtel@upslp.edu.mx</a></p>
+
+	<p>Coordinación de Ingeniería en Tecnologías de la Información (ITI):
+	<br>Dr. Juan Antonio Cabrera Rico 
+	<a href=mailto:iti@upslp.edu.mx>iti@upslp.edu.mx</a></p>
+
+	<p>Coordinación de Ingeniería en Tecnologías de Manufactura (ITMA):
+	<br>Dr. Margarito Martínez Cruz
+	<a href=mailto:itma@upslp.edu.mx>itma@upslp.edu.mx</a></p>
+
+	<p>Coordinación de Ingeniería en Sistemas y Tecnologías Industriales (ISTI):
+	<br>Dra. Patricia Castillo Galván
+	<a href=mailto:isti@upslp.edu.mx>isti@upslp.edu.mx</a></p>
+
+	<p>Coordinación de la Licenciatura en Administración y Gestión (LAG):
+	<br>Dra. Gloria Cristina Palos Cerda
+	<a href=mailto:lag@upslp.edu.mx>lag@upslp.edu.mx</a></p>
+
+	<p>Coordinación de la Licenciatura en Mercadotecnia Internacional (LMI):
+	<br>Mtra. Xóchitl Hernández Velázquez
+	<a href=mailto:lmi@upslp.edu.mx>lmi@upslp.edu.mx</a></p>
+
+	<p>Academia de Matemáticas y Ciencias:
+	<br>Dr. Juan Arturo Hernández Morales
+	<br><a href=mailto:matematicas@upslp.edu.mx>matematicas@upslp.edu.mx</a>
+	<br><a href=mailto: ciencias@upslp.edu.mx>ciencias@upslp.edu.mx</a></p>
+
+	<p>Academia de Inglés: Mtra. Patricia García Ramos
+	<a href=mailto:cadi@upslp.edu.mx>cadi@upslp.edu.mx</a></p>
+
+	<p>Academia de Núcleo General:
+	<br>Mtra. Guadalupe del Socorro Palmer de los Santos
+	<a href=mailto:cadi@upslp.edu.mx>nucleo@upslp.edu.mx</a></p>
+
+	<p>Biblioteca (CID): 
+	 Lic. Alí Omar Mirabal Santillán
+	<a href=mailto:cid_upslp@upslp.edu.mx>cid_upslp@upslp.edu.mx</a></p>
+
+	<p>Centro de Autoaprendizaje del Idioma Inglés (CADI):
+	<a href=mailto:cadi.reservacion@upslp.edu.mx>cadi.reservacion@upslp.edu.mx</a></p>
+
+	<p>Centro Certificador MOS:
+	Mtra. María Teresa Reyes Harfush
+	<a href=mailto:certificaciones@upslp.edu.mx>certificaciones@upslp.edu.mx</a></p>
+	
+	<p>Academia de CISCO:
+	Dra. Liliana Gámez Zavala
+	<a href=mailto:academiadecisco@upslp.edu.mx>cademiadecisco@upslp.edu.mx</a></p>
+
+	<p>Centro de Nuevas Tecnologías (CNT): 
+	Dr. Juan Antonio Cabrera Rico
+	<a href=mailto:cnt@upslp.edu.mx>cnt@upslp.edu.mx</a></p>
+
+	<p>Centro Integral de Mercadotecnia y Medios Audiovisuales (CIMMA): 
+	Mtra. Xóchitl Hernández Velázquez
+	<a href=mailto:cimma@upslp.edu.mx>cimma@upslp.edu.mx</a></p>
+
+	<p>Apoyo a la Docencia: Lic. Alejandra Carpinteyro Zarazúa
+	<a href=mailto:apoyoaladocencia@upslp.edu.mx>apoyoaladocencia@upslp.edu.mx</a></p>
+
+	Becas: Lic. Alejandra Carpinteyro Zarazúa
+	<a href=mailto:becas_upslp@upslp.edu.mx>becas_upslp@upslp.edu.mx</a></p>
+
+	<p>Proyecto Institucional de Tutorías: Mtra. Verónica Hernández García
+	<a href=mailto:tutorias@upslp.edu.mx>tutorias@upslp.edu.mx</a></p>
+
+	<p>Departamento de Psicopedagogía: Lic. Bernardo Rangel Gómez
+	<a href=mailto:psicopedagogia@upslp.edu.mx>psicopedagogia@upslp.edu.mx</a></p>
+
+
+	<p>Calidad Institucional: Mtra. Adriana Acevedo Sánchez
+	<a href=mailto:calidad@upslp.edu.mx>calidad@upslp.edu.mx</a></p>
+
+	<p>Servicios de Salud: Lic. Alejandra Carpinteyro Zarazúa
+	<a href=mailto:serviciosdesalud@upslp.edu.mx>serviciosdesalud@upslp.edu.mx</a></p>
+
+	<p>Coordinación de Deportes: Roy Aldrett Lara
+	<a href=mailto:deportes@upslp.edu.mx>deportes@upslp.edu.mx</a></p>
+	
+
+	<p>Responsabilidad Social: Lic. Eva María Flores Ríos
+	<a href=mailto:responsabilidadsocial@upslp.edu.mx>responsabilidadsocial@upslp.edu.mx</a></p>
+
+	<p>Servicios Informáticos: Dra. Ingrid Guel Hernández
+	<a href=mailto:s.informaticos@upslp.edu.mx>s.informaticos@upslp.edu.mx</a></p>
+	
+
+	<p>Encargada de Despacho de Recursos Humanos: Mtra. Yadira Edith Brann González
+	<a href=mailto:recursoshumanos@upslp.edu.mx>recursoshumanos@upslp.edu.mx</a></p>
+	
+
+	<p>Recursos Materiales: Marco Antonio Arriaga Macías
+	<a href=mailto:r.materiales@upslp.edu.mx>r.materiales@upslp.edu.mx</a></p>
+	
+
+	<p>Adquisiciones: Lic. Jorge Barrera Sustaita
+	<a href=mailto:adquisiciones@upslp.edu.mx>adquisiciones@upslp.edu.mx</a><p>
+	
+
+	<p>Finanzas: C.P. Laura Cansino Bravo
+	<a href=mailto:finanzas@upslp.edu.mx>finanzas@upslp.edu.mx</a><p>
+
+	<p>Jurídico
+	<a href=mailto:juridico@upslp.edu.mx>juridico@upslp.edu.mx</a></p>`
+}
+function restaurarContenido() {
+	document.getElementById('content').innerHTML = ``;
+}
